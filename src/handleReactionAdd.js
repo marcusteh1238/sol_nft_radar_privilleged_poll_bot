7677475cad902e46a2ff9ad47eec1762ec5ed925
Teh Hao Rui Marcus <50549441+marcusteh1238@users.noreply.github.com>
2021-12-13 04:22:41 +0800

    oops

--- src/handleReactionAdd.js
+++ src/handleReactionAdd.js
@@ -1,8 +1,8 @@
-// const solNftRadarServerId = "892235360501923850";
-// // privilleged poll active in this channel.
-// const privillegedPollChannels = ["896571025125421066"];
-// // can vote if have any one of these roles.
-// const privillegedPollRoles = ["908565702099730504", "896571850153418773"];
+const solNftRadarServerId = "892235360501923850";
+// privilleged poll active in this channel.
+const privillegedPollChannels = ["896571025125421066"];
+// can vote if have any one of these roles.
+const privillegedPollRoles = ["908565702099730504", "896571850153418773"];
 
 async function handleReactionAdd(client, reaction, user) {
   if (
