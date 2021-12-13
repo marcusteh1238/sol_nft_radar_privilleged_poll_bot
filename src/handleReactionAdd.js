3fb30f3198b69adca2d1a7acdfad17ecbfa1ca87
Teh Hao Rui Marcus <50549441+marcusteh1238@users.noreply.github.com>
2021-12-13 13:42:08 +0800

    only remove the user's reaction

--- src/handleReactionAdd.js
+++ src/handleReactionAdd.js
@@ -14,7 +14,7 @@ async function handleReactionAdd(client, reaction, user) {
   const guild = await client.guilds.cache.get(reaction.message.guildId);
   const member = await guild.members.fetch(user.id);
   if (!member) return;
-  if (!member._roles || !containsOneIn(member._roles, privillegedPollRoles)) return reaction.remove();
+  if (!member._roles || !containsOneIn(member._roles, privillegedPollRoles)) return reaction.users.remove(user.id);
 }
 
 function containsOneIn(arr, toCheck) {
