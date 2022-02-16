536163d80c4331379e8ae57f6b6d7f706d93e947
Teh Hao Rui Marcus <50549441+marcusteh1238@users.noreply.github.com>
2022-02-16 11:45:53 +0800

    add radardao founder role to list"

--- src/handleReactionAdd.js
+++ src/handleReactionAdd.js
@@ -2,7 +2,7 @@ const solNftRadarServerId = "892235360501923850";
 // privilleged poll active in this channel.
 const privillegedPollChannels = ["896571025125421066"];
 // can vote if have any one of these roles.
-const privillegedPollRoles = ["908565702099730504", "896571850153418773"];
+const privillegedPollRoles = ["908565702099730504", "896571850153418773", "941515097799540767"];
 
 async function handleReactionAdd(client, reaction, user) {
   if (
