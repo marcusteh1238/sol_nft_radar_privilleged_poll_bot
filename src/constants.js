6930fd14b4787e04a146fb817768444fcb383e56
Teh Hao Rui Marcus <50549441+marcusteh1238@users.noreply.github.com>
2022-02-16 11:49:19 +0800

    move constants to separate file

---
+++ src/constants.js
@@ -0,0 +1,15 @@
+const solNftRadarServerId = "892235360501923850";
+// privilleged poll active in this channel.
+const privillegedPollChannels = ["896571025125421066"];
+// can vote if have any one of these roles.
+const privillegedPollRoles = [
+  "908565702099730504", 
+  "896571850153418773", 
+  "941515097799540767"
+];
+
+module.exports = {
+  solNftRadarServerId,
+  privillegedPollChannels,
+  privillegedPollRoles
+}
